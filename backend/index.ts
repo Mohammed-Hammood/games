fa0e71cc4036d16c7789c3da907e166e18313a97
Mohammed-Hammood <iraqiprofessionaldesigner@gmail.com>
2024-05-02 22:16:17 +0700

    added static files

--- backend/index.ts
+++ backend/index.ts
@@ -1,25 +1,45 @@
-import fastify, { FastifyReply, FastifyRequest } from "fastify";
-import { gamesList } from "./data.js";
-import cors from "@fastify/cors";
-// import path from "node:path";
-// import static from "@fastify/static";
+import express, { Request, Response, Express } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { gamesList } from './data';
+import path  from "node:path";
 
-const app = fastify({ logger: true });
 
-await app.register(cors, {
-    origin: ['http://localhost:3000', 'http://localhost:3001']
-})
-// app.register(require('@fastify/static'), {
-//     root: path.join(__dirname, 'public'),
-//     prefix: '/public/', // optional: default '/'
-//     index: false,
+// const path = require("path");
 
-//     constraints: { host: 'localhost' } // optional: default {}
-// })
+const app: Express = express();
 
-// app.get('/static', function (req, reply) {
-//     // reply.sendFile("public", path.join(__dirname, 'public', 'myHtml.html')) //directly
-// })
+app.use(express.static('public'));
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:5173",
+        "https://games-list.vercel.app",
+    ]
+}));
+
+
+// app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
+app.get("/", function (req: Request, res: Response) {
+    const html =  `<html><body>
+    <h1>This is the api for Games application</h1>
+    </body></html>`;
+    res.status(200);
+    res.send(html);
+    // res.sendFile(__dirname + "/views/pages/home.html");
+    // res.send('endpoints')
+});
+
 
 interface ParamsT {
     limit?: string,
@@ -30,12 +50,6 @@ interface ParamsT {
     language?: string,
 }
 
-app.get("/", (req, reply) => {
-    // return reply.send({
-    //     game: `https://${req.hostname}/api/games`,
-    // })
-    return reply.status(200).type("text/html").send(html);
-})
 
 app.get("/api/games", (req, reply) => {
     let games = gamesList;
@@ -64,6 +78,7 @@ app.get("/api/games", (req, reply) => {
 
     games = games.sort((a, b) => order === 'id' ? a.id - b.id : b.id - a.id).filter((_, i) => i >= min && i < max);
 
+
     const query_params = { limit, page, query, language, max, min, }
 
     reply.send({
@@ -75,68 +90,11 @@ app.get("/api/games", (req, reply) => {
     })
 })
 
-// const port = parseInt(process.env.port || "3000");
 
-// app.listen({ port }, (err: any) => {
-//     if (err) {
-//         console.log(err);
-//         process.exit(1);
-//     }
-//     console.log(`The server is listening on http://localhost:${port}`)
-// })
-const html = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
-            />
-            <title>Vercel + Fastify Hello World</title>
-            <meta
-            name="description"
-            content="This is a starter template for Vercel + Fastify."
-            />
-        </head>
-        <body>
-            <h1>Vercel + Fastify Hello World</h1>
-            <p>
-            This is a starter template for Vercel + Fastify. Requests are
-            rewritten from <code>/*</code> to <code>/api/*</code>, which runs
-            as a Vercel Function.
-            </p>
-            <p>
-                For example, here is the boilerplate code for this route:
-            </p>
-            <pre>
-        <code>import Fastify from 'fastify'
+const port = process.env.PORT || 5000;
 
-        const app = Fastify({
-        logger: true,
-        })
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
 
-        app.get('/', async (req, res) => {
-        return res.status(200).type('text/html').send(html)
-        })
-
-        export default async function handler(req: any, res: any) {
-        await app.ready()
-        app.server.emit('request', req, res)
-        }</code>
-            </pre>
-            <p>
-            <p>
-            <a href="https://vercel.com/templates/other/fastify-serverless-function">
-            Deploy your own
-            </a>
-            to get started.
-        </body>
-        </html>
-`;
-export default async function handler(req:FastifyRequest, reply:FastifyReply) {
-    await app.ready()
-    app.server.emit('request', req, reply)
-}
-
+module.exports = app;

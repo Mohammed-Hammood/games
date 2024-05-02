6aee434c25cca4a6569fb7ebb30cd984e5780512
Mohammed-Hammood <iraqiprofessionaldesigner@gmail.com>
2024-05-02 17:15:12 +0700

    first init

---
+++ backend/src/data.ts
@@ -0,0 +1,83 @@
+
+export const gamesList = [
+	{
+		id: 1,
+		title: "Baldur's Gate 3",
+		image: "/images/Baldurs-Gate-3.png",
+		description: "An ancient evil has returned to Baldur's Gate, intent on devouring it from the inside out. The fate of Faerun lies in your hands. Alone, you may resist. But together, you can overcome.",
+		rating: 2,
+		developers: ['larian studios'],
+		platforms: ["Mac", "PlayStation 5", "Xbox Series X|S","PC (Microsoft Windows)", "Google Stadia"],
+		genres: ["Adventure","Role-playing (RPG)","Strategy", "Tactical", "Turn-based strategy (TBS)"],
+		initial_release:"06-09-2020",
+		want_to_play_count: 100,
+		played_count: 55,
+		playing_count: 123,
+		screenshots: [],
+		offline_mode: {
+			number_of_players: 3
+		},
+		online_mode: {
+			number_of_players: 4
+		},
+		available_languages: ["english", "russian"],
+		social_sites: [
+			{
+				url: "https://en.wikipedia.org/wiki/Baldur%27s_Gate_III",
+				title: "Wikipedia",
+				icon: "wikipedia"
+			},
+			{
+				url: "https://baldursgate3.game/",
+				title: "Official site",
+				icon: "official site"
+			}
+		],
+	},
+	{
+		id: 2,
+		title: 'Flash',
+		image: '/images/image-1.png',
+		description: "This is a description",
+		rating: 2,
+		developers: [],
+		platforms: [],
+		genres: [],
+		initial_release:"06-09-2020",
+		want_to_play_count: 100,
+		played_count: 55,
+		playing_count: 123,
+		screenshots: [],
+		offline_mode: {
+			number_of_players: 3
+		},
+		online_mode: {
+			number_of_players: 4
+		},
+		available_languages: ["russian"],
+		social_sites: [],
+	},
+	{
+		id: 3,
+		title: 'Flash',
+		image: '/images/image-1.png',
+		description: "This is a description",
+		rating: 2,
+		developers: [],
+		platforms: [],
+		genres: [],
+		initial_release:"06-09-2020",
+		want_to_play_count: 100,
+		played_count: 55,
+		playing_count: 123,
+		screenshots: [],
+		offline_mode: {
+			number_of_players: 3
+		},
+		online_mode: {
+			number_of_players: 4
+		},
+		available_languages: ["english", ],
+		social_sites: [],
+	}
+]
---
+++ backend/src/index.ts
@@ -0,0 +1,79 @@
+import fastify from "fastify";
+import path from "node:path";
+import { gamesList } from "./data.js";
+import cors from "@fastify/cors";
+// import static from "@fastify/static";
+
+const app = fastify({ logger: true });
+
+await app.register(cors, {
+    origin: ['http://localhost:3000', 'http://localhost:3001']
+})
+// app.register(require('@fastify/static'), {
+//     root: path.join(__dirname, 'public'),
+//     prefix: '/public/', // optional: default '/'
+//     index: false,
+
+//     constraints: { host: 'localhost' } // optional: default {}
+// })
+
+// app.get('/static', function (req, reply) {
+//     // reply.sendFile("public", path.join(__dirname, 'public', 'myHtml.html')) //directly
+// })
+
+interface ParamsT {
+    limit?: string,
+    query?: string,
+    page?: string,
+    category?: string,
+    order?: "id" | "-id",
+    language?: string,
+}
+
+app.get("/api/games", (req, reply) => {
+    let games = gamesList;
+    const { query, language, page: _p, limit: _l, order } = req.query as ParamsT
+
+    const page = parseInt(_p ?? '1');
+    let limit = parseInt(_l ?? '10');
+
+    if (limit > 100) limit = 10;
+    const max = page * limit;
+    const min = max - limit;
+
+    if (query && query.trim().length > 0) {
+        const search = (text: string) => query.trim().toLowerCase().includes(text) || text.toLowerCase().includes(query.toLowerCase());
+
+        games = games.filter(item => search(item.description) || search(item.title) || item.developers.some(d => search(d)));
+
+    }
+
+    if (language && language.trim().length > 0 && language.toLowerCase() !== 'all') {
+
+        games = games.filter(item => item.available_languages.some(L => L.toLowerCase().includes(language)));
+
+    }
+    const games_count = games.length;
+
+    games = games.sort((a, b) => order === 'id' ? a.id - b.id : b.id - a.id).filter((_, i) => i >= min && i < max);
+
+    const query_params = { limit, page, query, language, max, min,}
+    
+    reply.send({
+        query_params,
+        ok: true,
+        games_count,
+        status: 200,
+        games,
+    })
+})
+
+const port = parseInt(process.env.port || "3000");
+
+app.listen({ port }, (err: any) => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log(`The server is listening on http://localhost:${port}`)
+})

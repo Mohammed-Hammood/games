63b2821df64e0efee56163f2b40a0c727cc9f898
Mohammed-Hammood <iraqiprofessionaldesigner@gmail.com>
2024-05-02 18:14:48 +0700

    added home page

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,10 @@ await app.register(cors, {
     origin: ['http://localhost:3000', 'http://localhost:3001']
 });
 app.get("/", (req, reply) => {
-    return reply.send({
-        game: `https://${req.hostname}/api/games`,
-    });
+    // return reply.send({
+    //     game: `https://${req.hostname}/api/games`,
+    // })
+    return reply.status(200).type("text/html").send(html);
 });
 app.get("/api/games", (req, reply) => {
     let games = gamesList;
@@ -39,11 +40,66 @@ app.get("/api/games", (req, reply) => {
         games,
     });
 });
-const port = parseInt(process.env.port || "3000");
-app.listen({ port }, (err) => {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    console.log(`The server is listening on http://localhost:${port}`);
-});
+// const port = parseInt(process.env.port || "3000");
+// app.listen({ port }, (err: any) => {
+//     if (err) {
+//         console.log(err);
+//         process.exit(1);
+//     }
+//     console.log(`The server is listening on http://localhost:${port}`)
+// })
+export default async function handler(req, reply) {
+    await app.ready();
+    app.server.emit('request', req, reply);
+}
+const html = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
+            />
+            <title>Vercel + Fastify Hello World</title>
+            <meta
+            name="description"
+            content="This is a starter template for Vercel + Fastify."
+            />
+        </head>
+        <body>
+            <h1>Vercel + Fastify Hello World</h1>
+            <p>
+            This is a starter template for Vercel + Fastify. Requests are
+            rewritten from <code>/*</code> to <code>/api/*</code>, which runs
+            as a Vercel Function.
+            </p>
+            <p>
+                For example, here is the boilerplate code for this route:
+            </p>
+            <pre>
+        <code>import Fastify from 'fastify'
+
+        const app = Fastify({
+        logger: true,
+        })
+
+        app.get('/', async (req, res) => {
+        return res.status(200).type('text/html').send(html)
+        })
+
+        export default async function handler(req: any, res: any) {
+        await app.ready()
+        app.server.emit('request', req, res)
+        }</code>
+            </pre>
+            <p>
+            <p>
+            <a href="https://vercel.com/templates/other/fastify-serverless-function">
+            Deploy your own
+            </a>
+            to get started.
+        </body>
+        </html>
+`;
--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { gamesList } from "./data.js";
 import cors from "@fastify/cors";
 // import path from "node:path";
@@ -31,9 +31,10 @@ interface ParamsT {
 }
 
 app.get("/", (req, reply) => {
-    return reply.send({
-        game: `https://${req.hostname}/api/games`,
-    })
+    // return reply.send({
+    //     game: `https://${req.hostname}/api/games`,
+    // })
+    return reply.status(200).type("text/html").send(html);
 })
 
 app.get("/api/games", (req, reply) => {
@@ -74,12 +75,68 @@ app.get("/api/games", (req, reply) => {
     })
 })
 
-const port = parseInt(process.env.port || "3000");
+// const port = parseInt(process.env.port || "3000");
 
-app.listen({ port }, (err: any) => {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    console.log(`The server is listening on http://localhost:${port}`)
-})
+// app.listen({ port }, (err: any) => {
+//     if (err) {
+//         console.log(err);
+//         process.exit(1);
+//     }
+//     console.log(`The server is listening on http://localhost:${port}`)
+// })
+const html = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
+            />
+            <title>Vercel + Fastify Hello World</title>
+            <meta
+            name="description"
+            content="This is a starter template for Vercel + Fastify."
+            />
+        </head>
+        <body>
+            <h1>Vercel + Fastify Hello World</h1>
+            <p>
+            This is a starter template for Vercel + Fastify. Requests are
+            rewritten from <code>/*</code> to <code>/api/*</code>, which runs
+            as a Vercel Function.
+            </p>
+            <p>
+                For example, here is the boilerplate code for this route:
+            </p>
+            <pre>
+        <code>import Fastify from 'fastify'
+
+        const app = Fastify({
+        logger: true,
+        })
+
+        app.get('/', async (req, res) => {
+        return res.status(200).type('text/html').send(html)
+        })
+
+        export default async function handler(req: any, res: any) {
+        await app.ready()
+        app.server.emit('request', req, res)
+        }</code>
+            </pre>
+            <p>
+            <p>
+            <a href="https://vercel.com/templates/other/fastify-serverless-function">
+            Deploy your own
+            </a>
+            to get started.
+        </body>
+        </html>
+`;
+export default async function handler(req:FastifyRequest, reply:FastifyReply) {
+    await app.ready()
+    app.server.emit('request', req, reply)
+}
+
